40a5da01133acbe2910637ed037f8e42af7ba8a3
Temi A <temiakinyoade@gmail.com>
2023-02-17 10:50:02 -0500

    initial commit

---
+++ textclassify_model.py
@@ -0,0 +1,233 @@
+# STEP 1: rename this file to textclassify_model.py
+
+# feel free to include more imports as needed here
+# these are the ones that we used for the base model
+import numpy as np
+import sys
+from collections import Counter
+import math
+
+"""
+Your name and file comment here:
+"""
+
+
+"""
+Cite your sources here:
+"""
+
+"""
+Implement your functions that are not methods of the TextClassify class here
+"""
+def generate_tuples_from_file(training_file_path):
+  """
+  Generates tuples from file formated like:
+  id\ttext\tlabel
+  Parameters:
+    training_file_path - str path to file to read in
+  Return:
+    a list of tuples of strings formatted [(id, example_text, label), (id, example_text, label)....]
+  """
+  f = open(training_file_path, "r", encoding="utf8")
+  listOfExamples = []
+  for review in f:
+    if len(review.strip()) == 0:
+      continue
+    dataInReview = review.split("\t")
+    for i in range(len(dataInReview)):
+      # remove any extraneous whitespace
+      dataInReview[i] = dataInReview[i].strip()
+    t = tuple(dataInReview)
+    listOfExamples.append(t)
+  f.close()
+  return listOfExamples
+
+def precision(gold_labels, predicted_labels):
+  """
+  Calculates the precision for a set of predicted labels give the gold (ground truth) labels.
+  Parameters:
+      gold_labels (list): a list of labels assigned by hand ("truth")
+      predicted_labels (list): a corresponding list of labels predicted by the system
+  Returns: double precision (a number from 0 to 1)
+  """
+  pass
+
+
+def recall(gold_labels, predicted_labels):
+  """
+  Calculates the recall for a set of predicted labels give the gold (ground truth) labels.
+  Parameters:
+      gold_labels (list): a list of labels assigned by hand ("truth")
+      predicted_labels (list): a corresponding list of labels predicted by the system
+  Returns: double recall (a number from 0 to 1)
+  """
+  pass
+
+def f1(gold_labels, predicted_labels):
+  """
+  Calculates the f1 for a set of predicted labels give the gold (ground truth) labels.
+  Parameters:
+      gold_labels (list): a list of labels assigned by hand ("truth")
+      predicted_labels (list): a corresponding list of labels predicted by the system
+  Returns: double f1 (a number from 0 to 1)
+  """
+  pass
+
+
+"""
+Implement any other non-required functions here
+"""
+
+
+
+"""
+implement your TextClassify class here
+"""
+class TextClassify:
+
+
+  def __init__(self):
+    # do whatever you need to do to set up your class here
+    pass
+
+  def train(self, examples):
+    """
+    Trains the classifier based on the given examples
+    Parameters:
+      examples - a list of tuples of strings formatted [(id, example_text, label), (id, example_text, label)....]
+    Return: None
+    """
+    pass
+
+  def score(self, data):
+    """
+    Score a given piece of text
+    Parameters:
+      data - str like "I loved the hotel"
+    Return: dict of class: score mappings
+    """
+    pass
+
+  def classify(self, data):
+    """
+    Label a given piece of text
+    Parameters:
+      data - str like "I loved the hotel"
+    Return: string class label
+    """
+    pass
+
+  def featurize(self, data):
+    """
+    we use this format to make implementation of your TextClassifyImproved model more straightforward and to be 
+    consistent with what you see in nltk
+    Parameters:
+      data - str like "I loved the hotel"
+    Return: a list of tuples linking features to values
+    for BoW, a list of tuples linking every word to True [("I", True), ("loved", True), ("it", True)]
+    """
+    pass
+
+  def __str__(self):
+    return "Naive Bayes - bag-of-words baseline"
+
+
+class TextClassifyImproved:
+
+  def __init__(self):
+    pass
+
+  def train(self, examples):
+    """
+    Trains the classifier based on the given examples
+    Parameters:
+      examples - a list of tuples of strings formatted [(id, example_text, label), (id, example_text, label)....]
+    Return: None
+    """
+    pass
+
+  def score(self, data):
+    """
+    Score a given piece of text
+    you’ll compute e ^ (log(p(c)) + sum(log(p(w_i | c))) here
+    
+    Parameters:
+      data - str like "I loved the hotel"
+    Return: dict of class: score mappings
+    return a dictionary of the values of P(data | c)  for each class, 
+    as in section 4.3 of the textbook e.g. {"0": 0.000061, "1": 0.000032}
+    """
+    pass
+
+  def classify(self, data):
+    """
+    Label a given piece of text
+    Parameters:
+      data - str like "I loved the hotel"
+    Return: string class label
+    """
+    pass
+
+  def featurize(self, data):
+    """
+    we use this format to make implementation of this class more straightforward and to be 
+    consistent with what you see in nltk
+    Parameters:
+      data - str like "I loved the hotel"
+    Return: a list of tuples linking features to values
+    for BoW, a list of tuples linking every word to True [("I", True), ("loved", True), ("it", True)]
+    """
+    pass
+
+  def __str__(self):
+    return "NAME OF YOUR CLASSIFIER HERE"
+
+
+  def describe_experiments(self):
+    s = """
+    Description of your experiments and their outcomes here.
+    """
+    return s
+
+
+
+def main():
+
+  training = sys.argv[1]
+  testing = sys.argv[2]
+
+  classifier = TextClassify()
+  print(classifier)
+  # do the things that you need to with your base class
+
+
+  # report precision, recall, f1
+  
+
+  improved = TextClassifyImproved()
+  print(improved)
+  # do the things that you need to with your improved class
+
+
+  # report final precision, recall, f1 (for your best model)
+
+
+  # report a summary of your experiments/features here
+  print(improved.describe_experiments())
+
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage:", "python textclassify_model.py training-file.txt testing-file.txt")
+    sys.exit(1)
+
+  main()
+ 
+
+
+
+
+
+
+
+
